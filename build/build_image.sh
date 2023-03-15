daf0e6130b5b8c48cff6a1fda75db20c7745dd2e
dvaumoron <dvaumoron@gmail.com>
2023-03-15 17:08:32 +0100

    add build_image.sh

---
+++ build/build_image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+go install
+
+buildah from --name puzzlesettingsserver-working-container scratch
+buildah copy puzzlesettingsserver-working-container $HOME/go/bin/puzzlesettingsserver /bin/puzzlesettingsserver
+buildah config --env SERVICE_PORT=50051 puzzlesettingsserver-working-container
+buildah config --port 50051 puzzlesettingsserver-working-container
+buildah config --entrypoint '["/bin/puzzlesettingsserver"]' puzzlesettingsserver-working-container
+buildah commit puzzlesettingsserver-working-container puzzlesettingsserver
+buildah rm puzzlesettingsserver-working-container
